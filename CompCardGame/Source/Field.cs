77266e29948fce7946fcdc708343f702c956e974
Piotr Jackowski <pjackowski18@gmail.com>
2021-02-01 16:19:33 -0600

    update
    minor change to field structure

--- CompCardGame/Source/Field.cs
+++ CompCardGame/Source/Field.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace CompCardGame.Source
 {
-    class Field: Drawable
+    class Field : Drawable
     {
         Card[] player1Field;
         Card[] player2Field;
@@ -26,7 +26,8 @@ namespace CompCardGame.Source
                 {
                     player1Field[fieldPosition] = card;
                     return true;
-                } else
+                }
+                else
                 {
                     return false;
                 }
@@ -47,9 +48,9 @@ namespace CompCardGame.Source
 
         public void Draw(RenderTarget target, RenderStates states)
         {
-            for(int i = 0; i < player1Field.Count();i++)
+            for (int i = 0; i < player1Field.Count(); i++)
             {
-                target.Draw(new RectangleShape(new SFML.System.Vector2f { X = 200f, Y = 320f }) { OutlineColor = Color.Green, OutlineThickness =1, FillColor = Color.Transparent , Position = new SFML.System.Vector2f { X = i * 220 + 200, Y = 600 } });
+                target.Draw(new RectangleShape(new SFML.System.Vector2f { X = 200f, Y = 320f }) { OutlineColor = Color.Green, OutlineThickness = 1, FillColor = Color.Transparent, Position = new SFML.System.Vector2f { X = i * 220 + 200, Y = 600 } });
             }
 
             for (int i = 0; i < player2Field.Count(); i++)
