bb25454c83a023650bf02ef72f00fc1a87dda49d
Piotr Jackowski <pjackowski18@gmail.com>
2021-02-06 23:02:21 -0600

    refactoring field

--- CompCardGame/Source/Field.cs
+++ CompCardGame/Source/Field.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using SFML.Graphics;
+using SFML.System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -50,13 +51,22 @@ namespace CompCardGame.Source
         {
             for (int i = 0; i < player1Field.Count(); i++)
             {
-                target.Draw(new RectangleShape(new SFML.System.Vector2f { X = 200f, Y = 320f }) { OutlineColor = Color.Green, OutlineThickness = 1, FillColor = Color.Transparent, Position = new SFML.System.Vector2f { X = i * 220 + 200, Y = 600 } });
+                target.Draw(CardOutlineRectangle(i * 220 + 200, 600));
             }
 
             for (int i = 0; i < player2Field.Count(); i++)
             {
-                target.Draw(new RectangleShape(new SFML.System.Vector2f { X = 200f, Y = 320f }) { OutlineColor = Color.Green, OutlineThickness = 1, FillColor = Color.Transparent, Position = new SFML.System.Vector2f { X = i * 220 + 200, Y = 100 } });
+                //target.Draw(new RectangleShape(new SFML.System.Vector2f { X = 200f, Y = 320f }) { OutlineColor = Color.Green, OutlineThickness = 1, FillColor = Color.Transparent, Position = new SFML.System.Vector2f { X = i * 220 + 200, Y = 100 } });
+                target.Draw(CardOutlineRectangle(i * 220 + 200, 100));
             }
+
+            target.Draw(CardOutlineRectangle(1400,600));
+        }
+
+        //This is a helper to reduce the annoyance of drawing the outlines
+        private static RectangleShape CardOutlineRectangle(float x, float y)
+        {
+            return new RectangleShape(new Vector2f { X = 200f, Y = 320f }) { OutlineColor = Color.Green, OutlineThickness = 1, FillColor = Color.Transparent, Position = new Vector2f { X = x, Y = y } };
         }
     }
 }
