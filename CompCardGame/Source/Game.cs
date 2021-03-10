f56fab87bc79e27811f36228483a00e9a9aad450
Piotr Jackowski <pjackowski18@gmail.com>
2021-03-10 04:15:38 -0600

    re factored stuff related to match into its own class

--- CompCardGame/Source/Game.cs
+++ CompCardGame/Source/Game.cs
@@ -5,33 +5,26 @@ using System.Collections.Generic;
 
 namespace CompCardGame.Source
 {
-    enum TurnState //this is so we can make state machine so we know which turn phase it is
+    enum GameState
     {
-        Drawing,
-        Primary,
-        Attack,
-        Secondary,
-        End
-    }
-
-    enum GameState // this is so we can make a state machine so we know whos turn it is
-    {
-        Player,
-        Opponent
+        Loading,
+        MainPage,
+        Match,
+        Settings
     }
     class Game
     {
 
-        RenderWindow window; //Essentially what you are drawing too
-        List<Card> cards = new List<Card>();//temporary
+        private RenderWindow window; //Essentially what you are drawing too
 
-        Player player1;
-        Player player2;
-        Field gameField;
+        private Match match;
 
-        public static TurnState turnState;
+        public static GameState GameState;
 
-        private Card selectedCard;
+        
+
+        public static InputHandler InputHandler = new InputHandler();
+        //private Card selectedCard;
         //drawing the field and positioning of the cards on the field will be dependent on these two
         public static uint ScreenWidth { get; private set; }
         public static uint ScreenHeight { get; private set; }
@@ -47,26 +40,20 @@ namespace CompCardGame.Source
             //this is how events are handled we will probably just need esc on keyboard and mouse movement/clicking
             window.Closed += new EventHandler(OnClose);
             //handler for mouseMovement
-            window.MouseMoved += new EventHandler<MouseMoveEventArgs>(MouseMovement);
+            window.MouseMoved += new EventHandler<MouseMoveEventArgs>(InputHandler.MouseMovement);
             //handler for mouseclick
-            window.MouseButtonPressed += new EventHandler<MouseButtonEventArgs>(MouseClick);
+            window.MouseButtonPressed += new EventHandler<MouseButtonEventArgs>(InputHandler.MouseClick);
 
-            window.MouseButtonReleased += new EventHandler<MouseButtonEventArgs>(MouseReleased);
-            //just initializing the fields, this will be moved into seperate code for starting a match
-            player1 = new Player(PlayerType.Player);
-            player2 = new Player(PlayerType.Enemy);
-            gameField = new Field();
-            player1.SetDeckPosition();
-            player2.SetDeckPosition();
+            window.MouseButtonReleased += new EventHandler<MouseButtonEventArgs>(InputHandler.MouseReleased);
+
+            GameState = GameState.Match;
+            //temporary this will be later connected to a button on the main page screen
+            match = new Match(new Player(PlayerType.Player), new Player(PlayerType.Enemy), window);
+            InputHandler.SetMatch(match);
             
-            //draw 3 cards from the deck and add to hand
-            for(int i = 0; i < 3;i++)
-            {
-                player1.DrawACardFromDeck();
-                player2.DrawACardFromDeck();
-            }
-            gameField.PlaceCardOnField(PlayerType.Enemy, gameField.GetRandomFieldPosition(PlayerType.Enemy), player2.GrabRandomCard());//temporary
-            turnState = TurnState.Primary;
+
+            
+    
         }
         public void Run()
         {
@@ -85,6 +72,22 @@ namespace CompCardGame.Source
 
         private void Update()
         {
+
+            switch (GameState)
+            {
+                case GameState.Loading:
+                    window.Draw(new RectangleShape(new SFML.System.Vector2f(ScreenWidth, ScreenHeight)));
+                    break;
+                case GameState.MainPage:
+                    break;
+                case GameState.Match:
+                    match.Update();
+                    break;
+                case GameState.Settings:
+                    break;
+                default:
+                    break;
+            }
             //if (!Mouse.IsButtonPressed(Mouse.Button.Left))
             //{
             // player1.ResetCards();
@@ -94,7 +97,7 @@ namespace CompCardGame.Source
             //    X = 10f,
             //    Y = 10f
             //};
-            
+
             //method to draw arrow to mouse from attacking card
         }
         private void Render()
@@ -103,14 +106,32 @@ namespace CompCardGame.Source
             //I think we need to setup a view so that sizing doesn't go weird if you resize the game
             //these are the individual calls to each class for drawing what they need to draw
             //Remember order matters for drawing
-            window.Draw(gameField);
-            
-            window.Draw(player2);
-            window.Draw(player1);
-            if (selectedCard != null)
+            switch(GameState)
             {
-                window.Draw(selectedCard);
+                case GameState.Loading:
+                    window.Draw(new RectangleShape(new SFML.System.Vector2f(ScreenWidth, ScreenHeight)));
+                    break;
+                case GameState.MainPage:
+                    window.Draw(InputHandler);
+                    
+                    break;
+                case GameState.Match:
+                    match.Render();
+                    window.Draw(InputHandler);
+                    break;
+                case GameState.Settings:
+                    break;
+                default:
+                    break;
             }
+            //window.Draw(gameField);
+            
+            //window.Draw(player2);
+            //window.Draw(player1);
+            //if (selectedCard != null)
+            //{
+            //    window.Draw(selectedCard);
+            //}
             window.Display();
         }
 
@@ -120,138 +141,17 @@ namespace CompCardGame.Source
             RenderWindow window = (RenderWindow)sender;
             window.Close();
         }
-        //handles mouseMovement
-        private void MouseMovement(object sender, MouseMoveEventArgs e)
-        {
-            var mouse = new SFML.System.Vector2f(e.X, e.Y);
-            player1.HandleMouseMovement(mouse);
-            //TODO add handler for when card is selected and being moved
-            //if when mouse is clicked and moving a card
-            if (selectedCard != null && selectedCard.Location == CardLocation.Moving)
-            {
-                //needs to be shifted so mouse in middle of the card
-                selectedCard.UpdatePositions(mouse);
-            }
-        }
-
-        //this will be important for handling when card is placed on field and when attacking opponent
-        private void MouseReleased(object sender, MouseButtonEventArgs e)
-        {
-            //i wasn't sure if it was glitching so i made it run separate in a thread
-            System.Threading.Thread release = new System.Threading.Thread(new System.Threading.ThreadStart(() => 
-            {
-                //System.Threading.Thread.Sleep(5);
-                Tuple<PlayerType, FieldPosition> target;
-                var mouse = new SFML.System.Vector2f(e.X, e.Y);
-
-                target = gameField.GetTarget(mouse);
-                
-                if (selectedCard != null && target != null)//
-                {
-                    //handle releasing on player field position this will include spell handling
-                    if (target.Item1 == PlayerType.Player)
-                    {
-                        if (!target.Item2.HasCard && selectedCard.Location == CardLocation.Moving)
-                        {
-                            gameField.PlaceCardOnField(target.Item1, target.Item2, selectedCard);
-                            target.Item2.Card = selectedCard;
-                            selectedCard.Location = CardLocation.Field;
-                            selectedCard.Active = false;
-                            player1.RemoveCard(selectedCard);
-                            selectedCard = null;
-                            
-                            
-                        }
-                    } 
-                    else //handle release on opponent field in necessary
-                    {
-
-                    }
-                }
-                else if (selectedCard != null && target == null) //check where to place card when mouse is released and if no valid slot is selected then drop the card back to previous spot
-                {
-                    if (selectedCard.Location == CardLocation.Moving)
-                    {
-                        selectedCard.Active = false;
-                        selectedCard.Location = CardLocation.Hand;
-                        //player1.ResetCardPosition(selectedCard);
-                        selectedCard.ResetCard();
-                        selectedCard = null;
-                        //Console.WriteLine("released");
-
-                        //player1.ResetCards();
-                    }
-                    
-                }
-            }));
-            release.Start();
-            
-            
-        }
-
-
-        private void CalculateCardAttack(Card card1, Card card2) // handle attack between cards 
-        {
-            Console.WriteLine($"{card1} attacked {card2}");
-            if (card1.Attack > card2.Defense)//attack worked, send card2 to graveyard and deal difference of attack - defense to opponent
-            {
-
-            }
-            else //attack failed you get dealt difference of defense -attack
-            {
-
-            }
-        }
 
         
-        private void MouseClick(object sender, MouseButtonEventArgs e)//this might need the state of the turn in future
-        {
-            if (e.Button == Mouse.Button.Left)
-            {
-                //TODO add handler for player1
-                //Console.WriteLine($"{e.X}, {e.Y}");
-                var mouse = new SFML.System.Vector2f(e.X, e.Y);
-                if (turnState == TurnState.Attack)//temporary state check to test things out a lot wil lchange when we implement the turns
-                {
-                   var temp = gameField.SelectCard(mouse); //check field
-                    if (temp != null)
-                    {
-                        selectedCard = temp;
-                    } 
-                    else
-                    {
-                        Tuple<PlayerType, FieldPosition> target;
-                        target = gameField.GetTarget(mouse);
-                        if (target != null) //handle attacking card
-                        {
-                            CalculateCardAttack(selectedCard, target.Item2.Card);
-                        } else //attack hp directly
-                        {
-                            Console.WriteLine("Attack health");
-                        }
-                    }
-                } 
-                else
-                {
-                    selectedCard = player1.HandleMouseClick(mouse);//check hand
-                    if (selectedCard != null)
-                    {
-                        selectedCard.Location = CardLocation.Moving;
-                    }
-                }
-                
-                if (selectedCard == null)
-                {
-                    selectedCard = gameField.SelectCard(mouse); //check field
-                }
-                
-                
-            }
-            else if (e.Button == Mouse.Button.Right)//temporary for testing
-            {
-                turnState = TurnState.Attack;
-            }
-        }
+        
+
+        
+
+
+        
+
+        
+        
     }
 }
 
---
+++ CompCardGame/Source/Match.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Window;
+using SFML.Graphics;
+using SFML.System;
+
+namespace CompCardGame.Source
+{
+
+    #region StateMachine Enumerators
+    enum TurnState //this is so we can make state machine so we know which turn phase it is
+    {
+        Drawing,
+        Primary,
+        Attack,
+        Secondary,
+        End
+    }
+
+    enum MatchState // this is so we can make a state machine so we know whos turn it is
+    {
+        Player,
+        Opponent
+    }
+    #endregion
+    class Match
+    {
+
+        
+        private RenderWindow window;
+        
+        private readonly Player[] players;
+        private readonly Field field;
+
+        public static TurnState TurnState;
+        public static MatchState MatchState;
+
+        private Card selectedCard;
+        private Queue<Card> cardsToRemove = new Queue<Card>();
+
+        public Match(Player player1, Player player2, RenderWindow window)
+        {
+            //initialize field and players by setting positions and drawing 3 cards
+            this.window = window;
+            players = new Player[2] {player1, player2};
+            field = new Field();
+
+            foreach (var player in players)
+            {
+                player.SetDeckPosition();
+                for (int i = 0; i < 3; i++)
+                {
+                    player.DrawACardFromDeck();
+                }
+            }
+            field.PlaceCardOnField(PlayerType.Enemy, field.GetRandomFieldPosition(PlayerType.Enemy), player2.GrabRandomCard());//temporary
+            TurnState = TurnState.Primary;//temporary
+
+            Game.InputHandler.AddButton(new Button("test Button",20, new Vector2f(1920 / 2-100, 1080 / 2-50), 200, 100, Color.Black));
+        }
+
+        public void Render()
+        {
+            window.Draw(field);
+            
+            foreach(var player in players)
+            {
+                window.Draw(player);
+            }
+
+            if (selectedCard != null)
+            {
+                window.Draw(selectedCard);
+            }
+        }
+
+
+        public void MouseMovement(Vector2f mouse)
+        {
+            players[0].HandleMouseMovement(mouse);
+
+            if (selectedCard != null && selectedCard.Location == CardLocation.Moving)
+            {
+                //needs to be shifted so mouse in middle of the card
+                selectedCard.UpdatePositions(mouse);
+            }
+        }
+
+        public void MouseReleased(Vector2f mouse)
+        {
+            switch(TurnState)
+            {
+                case TurnState.Primary:
+                    Tuple<PlayerType, FieldPosition> target = field.GetTarget(mouse);
+
+                    if (selectedCard != null && target != null)//
+                    {
+                        //handle releasing on player field position this will include spell handling
+                        if (target.Item1 == PlayerType.Player)
+                        {
+                            if (!target.Item2.HasCard && selectedCard.Location == CardLocation.Moving)
+                            {
+                                field.PlaceCardOnField(target.Item1, target.Item2, selectedCard);
+                                target.Item2.Card = selectedCard;
+                                selectedCard.Location = CardLocation.Field;
+                                selectedCard.Active = false;
+                                cardsToRemove.Enqueue(selectedCard);
+                                //players[0].RemoveCard(selectedCard);
+                                selectedCard = null;
+
+
+                            }
+                        }
+                        else //handle release on opponent field in necessary
+                        {
+
+                        }
+                    }
+                    else if (selectedCard != null && target == null) //check where to place card when mouse is released and if no valid slot is selected then drop the card back to previous spot
+                    {
+                        if (selectedCard.Location == CardLocation.Moving)
+                        {
+                            selectedCard.Active = false;
+                            selectedCard.Location = CardLocation.Hand;
+                            //player1.ResetCardPosition(selectedCard);
+                            selectedCard.ResetCard();
+                            selectedCard = null;
+                            //Console.WriteLine("released");
+
+                            //player1.ResetCards();
+                        }
+
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+            
+
+        }
+
+        public void MouseClick(Vector2f mouse)
+        {
+            
+            //check buttons
+
+
+            switch (TurnState) //TODO: needs handler for each phase
+            {
+                case TurnState.Drawing:
+                    break;
+                case TurnState.Primary:
+                    
+                    
+                    selectedCard = players[0].HandleMouseClick(mouse);
+                   
+                    break;
+                case TurnState.Attack:
+                    var temp = field.SelectCard(mouse);
+                    if (temp != null)
+                    {
+                        selectedCard = temp;
+                    }
+                    else
+                    {
+                        Tuple<PlayerType, FieldPosition> target;
+                        target = field.GetTarget(mouse);
+                        if (target != null && target.Item1 == PlayerType.Enemy && selectedCard != null) //handle attacking card
+                        {
+                            CalculateCardAttack(selectedCard, target.Item2.Card);
+                        }
+                        else if (false)//TODO check area on top to attack health of player //attack hp directly
+                        {
+                            Console.WriteLine("Attack health");
+                        }
+                    }
+                    
+                    break;
+                case TurnState.Secondary:
+                    break;
+                case TurnState.End:
+                    break;
+                default:
+                    break;
+
+            }
+
+
+        }
+
+        public void Update()//multi player match might have to override this
+        {
+
+            switch(MatchState)//TODO 
+            {
+                case MatchState.Player:
+                    switch(TurnState)
+                    {
+                        case TurnState.Drawing://draw a card and give crystals to player
+                            break;
+                        case TurnState.Primary://player can place down cards check if he has crystals, use spells
+                            break;
+                        case TurnState.Attack://player can attack, use spells
+                            break;
+                        case TurnState.Secondary://player can use spells
+                            break;
+                        case TurnState.End://end turn apply status effects/field effects possibly
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case MatchState.Opponent://this is for regular match so we need AI code here so will need an AI class
+                    switch (TurnState)
+                    {
+                        case TurnState.Drawing://draw a card and give crystals to player
+
+                            break;
+                        case TurnState.Primary://player can place down cards check if he has crystals, use spells
+
+                            break;
+                        case TurnState.Attack://player can attack, use spells
+
+                            break;
+                        case TurnState.Secondary://player can use spells
+
+                            break;
+                        case TurnState.End://end turn apply status effects/field effects possibly
+
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            if (cardsToRemove.Count >0)
+            {
+                while (cardsToRemove.Count >0)
+                {
+                    var card = cardsToRemove.Dequeue();
+                    players[0].RemoveCard(selectedCard);
+                }
+            }
+        }
+
+        private void CalculateCardAttack(Card card1, Card card2) // handle attack between cards 
+        {
+            Console.WriteLine($"{card1} attacked {card2}");
+            if (card1.Attack > card2.Defense)//attack worked, send card2 to graveyard and deal difference of attack - defense to opponent
+            {
+
+            }
+            else //attack failed you get dealt difference of defense -attack
+            {
+
+            }
+        }
+
+
+        private void NextTurnState()
+        {
+            TurnState++;
+        }
+    }
+}
