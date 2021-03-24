5d5264aedf9c40fa2bc4fabc85db3d3411de839d
Piotr Jackowski <pjackowski18@gmail.com>
2021-03-24 03:04:54 -0500

    added function to help get center position of two objects

--- CompCardGame/Source/HelperFunctions.cs
+++ CompCardGame/Source/HelperFunctions.cs
@@ -20,5 +20,16 @@ namespace CompCardGame.Source
             return new Text() { DisplayedString = text, Font = font, CharacterSize = charSize, FillColor = fillColor, Position = position };
         }
 
+        //center the text
+        public static Vector2f GetCenteredPosition(Vector2f buttonSize, Vector2f textSize)
+        {
+            Console.WriteLine(textSize);
+            int x = ((int)buttonSize.X >> 1) - ((int)textSize.X >> 1);//bitshift is faster than division only works for evens this case we divide by 2
+            var y = buttonSize.Y / 2 - (textSize.Y);
+            Vector2f result = new Vector2f(x, y);//TODO calculate center
+
+            return result;
+        }
+
     }
 }
