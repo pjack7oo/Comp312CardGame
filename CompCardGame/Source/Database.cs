921ce75793b10d761ac9a0c879ff29a7bae104b6
DESKTOP-MFGOIAI\nnati <nnatindim>
2021-04-01 17:15:54 -0500

    class created to connect to database

---
+++ CompCardGame/Source/Database.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace CompCardGame.Source
+{
+    class Database
+    {
+        public void DBConnectionTest()
+        {
+            var client = new MongoClient("mongodb+srv://root:comp312@cluster0.kjv42.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+            var dbList = client.ListDatabases().ToList();
+
+            Console.WriteLine("The list of databases are:");
+            foreach (var item in dbList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
